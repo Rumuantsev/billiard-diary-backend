9db2ef36a57a3f36a9297ae5012b00561dfa51c2
Arseniy <git.user.ars@gmail.com>
2026-02-06 21:48:42 +0700

    add CRUD positions and docker

---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+const positionsRouter = require("./positions.controller");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/positions", positionsRouter);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Backend started on port", PORT);
+});
